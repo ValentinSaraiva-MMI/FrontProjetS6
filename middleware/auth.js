78e163035da9c338b20ce70009db5c05da5e6205
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2024-02-14 19:51:46 +0100

    back

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  if (process.server) return;
-  const token = localStorage.getItem("token");
-  console.log(token);
-  if (!token || token === "null") {
-    return navigateTo("/");
-  }
-});
