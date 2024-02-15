e36939a5a522fb362d6392314d2e0231798a4f1a
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2024-02-15 07:04:14 +0100

    new bdd

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -16,20 +16,20 @@ INSERT INTO MyCard (
         card_category
     )
 VALUES (
-        'Manger & bouger',
+        'Manger bouger',
         'https://source.unsplash.com/random/1000x1000/?fitness',
-        'Savez vous que même un peu de sport pouvait réduire le stress, améliorer la qualité du sommeil, et agir comme un antidépresseur ?',
+        'Savez vous que même peu de sport pouvait réduire le stress et agir comme un antidépresseur ?',
         'Fitness'
     ),
     (
         'Le Jardinage',
         'https://source.unsplash.com/random/1000x1000/?garden',
-        'Le jardinage réduit le stress et favorise le bien-être mental.',
+        'Le jardinage réduit le stress et favorise le bien-être mental. Il offre un refuge tranquille loin du tumulte de la vie quotidienne',
         'Main verte'
     ),
     (
         'Moins de stress',
-        'https://source.unsplash.com/random/1000x1000/?fitness',
+        'https://source.unsplash.com/random/1000x1000/?sport',
         "L'exercice physique aide à gérer le stress et à réduire l'anxiété.",
         'Fitness'
     ),
@@ -47,13 +47,13 @@ VALUES (
     ),
     (
         'Meilleur alimentation',
-        'https://source.unsplash.com/random/1000x1000/?garden',
+        'https://source.unsplash.com/random/1000x1000/?vegetables',
         "Cultiver ses propres fruits et légumes offre une source d'aliments frais et sains.",
         'Main verte'
     ),
     (
         'Être confiant',
         'https://source.unsplash.com/random/1000x1000/?sport',
-        "La pratique sportive renforce la confiance en soi et l'estime de soi.",
+        "La pratique sportive renforce la confiance en soi et l'estime de soi, ce qui en fait une aide mentale et physique",
         'Fitness'
     );
